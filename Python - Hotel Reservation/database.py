e617272aeb4c030e475a00dd5a29223f7b484f8b
Nandanagireeshh <155370273+Nandanagireeshh@users.noreply.github.com>
2024-10-20 00:46:58 +0530

    Add files via upload

--- Python - Hotel Reservation/database.py
+++ Python - Hotel Reservation/database.py
@@ -9,6 +9,8 @@ def select(q):
 	cur.close()
 	cnx.close()
 	return result
+
+
 def update(q):
 	cnx=mysql.connector.connect(user="root",password=password,host="localhost", database=database)
 	cur=cnx.cursor(dictionary=True)
@@ -18,6 +20,28 @@ def update(q):
 	cur.close()
 	cnx.close()
 	return result
+
+
+def updated(q, params):
+	# Establish the connection
+	cnx = mysql.connector.connect(user="root", password=password, host="localhost", database=database)
+	cur = cnx.cursor(dictionary=True)
+
+	# Execute the query with parameters
+	cur.execute(q, params)  # Pass parameters as a tuple
+	cnx.commit()  # Commit the changes
+
+	# Get the number of affected rows
+	result = cur.rowcount
+
+	# Close the cursor and connection
+	cur.close()
+	cnx.close()
+
+	# Return the number of affected rows
+	return result
+
+
 def delete(q):
 	cnx=mysql.connector.connect(user="root",password=password,host="localhost", database=database)
 	cur=cnx.cursor(dictionary=True)
@@ -45,4 +69,17 @@ def insert(q):
 	cnx.close()
 	return result
 
-	
+
+def inserted(q, params):
+    cnx = mysql.connector.connect(user="root", password=password, host="localhost", database=database)
+    cur = cnx.cursor()
+    cur.execute(q, params)  # Use params here
+    cnx.commit()
+    result = cur.lastrowid
+    cur.close()
+    cnx.close()
+    return result
+
+
+
+
